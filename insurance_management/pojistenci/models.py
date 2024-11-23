6d42200a78711718c626d7e3934d149c70ba6ac9
Tigra1 <idontsov@seznam.cz>
2024-11-23 15:13:44 +0100

    Update models.py

--- insurance_management/pojistenci/models.py
+++ insurance_management/pojistenci/models.py
@@ -8,7 +8,7 @@ class Pojisteny(models.Model):
     jmeno = models.CharField(max_length=100)
     prijmeni = models.CharField(max_length=100)
     vek = models.IntegerField()
-    telefon = models.CharField(max_length=15)
+    telefon = models.CharField(max_length=13)
 
     def __str__(self):
         return f"{self.jmeno} {self.prijmeni}, {self.vek} let, Telefon: {self.telefon}"
